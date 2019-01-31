3fc1ad31d34929b08bb15653dc38aa84ceacad47
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-31 17:30:36 -0500

    Used this 'this.tabItem' to create a new instance of the TabItem class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -11,10 +11,11 @@ class TabLink {
     
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab="${element.dataset.tab}"]`);
-    console.log(this.itemElement);
+    // console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.tabItem);
+    console.log(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
 
